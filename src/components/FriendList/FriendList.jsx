c2870000971e1d33340164265884fbf5b8907f0b
Viktoriiapon <viktoriyaponom@gmail.com>
2024-03-10 21:18:02 +0100

    add folder with item

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import FriendListItem from './FriendListItem';
+import FriendListItem from '../FriendListItem/FriendListItem';
 import './friend_module.css'
 
 const FriendList = ({ friend }) => {
